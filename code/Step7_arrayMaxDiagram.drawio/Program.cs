4b3ddd1095c83d25e70acd8aadeaaa43a3776fc7
Margarita <gorbashevarira@gmail.com>
2026-03-14 11:53:02 +0300

    Step 7 arrays

---
+++ code/Step7_arrayMaxDiagram.drawio/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Добро пожаловать!");
+int[] nameArray = { 5, 4, 5, 3, 4 };
+Console.WriteLine("Массив: " + String.Join(" ", nameArray));
+int maxElement = nameArray.Max();
+Console.WriteLine("Максимальный элемент массива = " + maxElement);
